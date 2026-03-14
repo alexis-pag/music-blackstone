e2bad65c618d063574f08f130a65700dbe532913
Alexis Pag <alexisp57140@gmail.com>
2026-03-14 09:13:14 +0100

    "Fix_Requirements_And_Remove_Build_Script"

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Mise à jour des paquets et installation de ffmpeg (nécessaire pour la lecture audio)
-apt-get update
-apt-get install -y ffmpeg
-
-# Installation des dépendances Python
-pip install -r requirements.txt
